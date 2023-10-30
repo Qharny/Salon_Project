43e9b43ed90498cbfa6c9e5be5c8dbbd8004f312
Qharny <cannycc1020@gmail.com>
2023-10-30 16:05:29 +0000

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	new file:   connection.php
    	new file:   database.php
    	new file:   query.sql
    	new file:   signup_database.php

---
+++ connection.php
@@ -0,0 +1,21 @@
+<?php
+    // Start a new or resume an existing session
+    session_start();
+    
+    // Database connection parameters
+    $host = "localhost";        // Database host
+    $user_name = "root";        // Database username
+    $pass = "";                // Database password
+    $db_name = "Salon_project";   // Database name
+
+    // Establish a connection to the database
+    $my_connection = mysqli_connect($host, $user_name, $pass, $db_name);
+
+    // Check if the connection was successful
+    // if($my_connection){
+    //     echo "Connected to database Successfully"; // Display a success message if connected
+    // }
+    // else{
+    //     die("Connection Failed" . mysqli_error()); // Display an error message if connection failed
+    // }
+?>
---
+++ query.sql
@@ -0,0 +1,18 @@
+create database Salon_project;
+
+use Salon_project;
+
+-- creating the signup table
+
+create table signup(
+    id int not null auto_increment,
+    FullName varchar(200) not null,
+    Username varchar(200) not null,
+    Email varchar(50) not null,
+    Password varchar(50) not null,
+    Confirm_Password varchar(50) not null,
+    primary key(id)
+);
+
+
+INSERT INTO signup (FullName, Username, Email, Password, Confirm_Password) VALUES ('Sam Ben','they@me.com','12w3','12w3')
