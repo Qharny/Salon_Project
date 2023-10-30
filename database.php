86da6aed95d14a6e33202f6a0cd7d35d09587b63
Qharny <cannycc1020@gmail.com>
2023-10-30 18:32:09 +0000

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   database.php

--- database.php
+++ database.php
@@ -6,13 +6,13 @@ if(isset($_POST['login'])){
     $name = mysqli_real_escape_string($my_connection, $_POST['mail']);
     $pass = mysqli_real_escape_string($my_connection, $_POST['password']);
 
-    $query = "SELECT * FROM signup WHERE Username = '$name' OR Email = '$mail'";
+    $query = "SELECT * FROM signup WHERE Username = '$name' OR Email = '$name'";
     $result = mysqli_query($my_connection, $query);
 
     if($result && mysqli_num_rows($result) > 0){
         $row = mysqli_fetch_assoc($result);
 
-        if(password_verify($pass, $row["Password"])){
+        if($pass == $row["Password"]){
             $_SESSION["login"] = true;
             $_SESSION["id"] = $row["id"];
             header("Location: ./homepage.html"); // Redirect to homepage after successful login
