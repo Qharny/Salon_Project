d6ce387454d8a9e34f5bbc6a4938b8271ecd31fd
Qharny <cannycc1020@gmail.com>
2023-10-30 16:09:51 +0000

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   query.sql

--- query.sql
+++ query.sql
@@ -15,6 +15,7 @@ create table signup(
 );
 
 
-INSERT INTO signup (FullName, Username, Email, Password, Confirm_Password) VALUES ('Sam Ben','they@me.com','12w3','12w3');
+INSERT INTO signup (FullName, Username, Email, Password, Confirm_Password) VALUES ('Sam Ben','SBen','they@me.com','12w3','12w3');
+INSERT INTO signup (FullName, Username, Email, Password, Confirm_Password) VALUES ('Kim Jim','Kim','ik@je.com','129','129');
 
 SELECT * FROM Signup;
