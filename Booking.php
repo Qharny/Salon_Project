b81542367e2e0a4b5682113b97f9f317a9adc108
Qharny <cannycc1020@gmail.com>
2023-10-31 18:47:15 +0000

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   Booking.php
    	new file:   booking.css
    	new file:   img/bg.jpg

--- Booking.php
+++ Booking.php
@@ -4,15 +4,31 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Booking || Area</title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78i">
+    <link rel="stylesheet" href="booking.css">
 </head>
 <body>
-<form method="post" action="booking.php">
+    <div class="secondary-header">
+        <div class="mail">
+            Get in touch: +233-24-000-0000 |<a href="#">CLick here for a <strong>BOOKING</strong></a> | info@salonproject.com
+        </div>
+        <div class="social">
+            <a href="#"><i class="fab fa-facebook"></i></a>
+            <a href="#"><i class="fab fa-twitter"></i></a>
+            <a href="#"><i class="fab fa-instagram"></i></a>
+        </div>
+    </div>
+<!-- <form method="post" action="booking.php">
     Name: <input type="text" name="name"><br>
     Email: <input type="text" name="email"><br>
     Date: <input type="date" name="date"><br>
     Time: <input type="time" name="time"><br>
     <input type="submit" name="submit" value="Book">
-</form>
+</form> -->
+
+<script src="https://kit.fontawesome.com/11daeabc17.js" crossorigin="anonymous"></script>
 </body>
 </html>
 <?php
