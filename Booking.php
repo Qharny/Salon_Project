99bed71cf1ba07f6b8b96defc059e066277319b3
Qharny <cannycc1020@gmail.com>
2023-11-01 11:27:58 +0000

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is behind 'origin/main' by 3 commits, and can be fast-forwarded.
      (use "git pull" to update your local branch)
    
    Changes to be committed:
    	modified:   Booking.php
    	modified:   booking.css

--- Booking.php
+++ Booking.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,6 +10,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78i">
     <link rel="stylesheet" href="booking.css">
 </head>
+
 <body>
     <div class="secondary-header">
         <div class="mail">
@@ -20,31 +22,52 @@
             <a href="#"><i class="fab fa-instagram"></i></a>
         </div>
     </div>
-<!-- <form method="post" action="booking.php">
-    Name: <input type="text" name="name"><br>
-    Email: <input type="text" name="email"><br>
-    Date: <input type="date" name="date"><br>
-    Time: <input type="time" name="time"><br>
-    <input type="submit" name="submit" value="Book">
-</form> -->
+    <header>
+        <div class="logo">
+            <img src='https://www.w3schools.com/howto/img_avatar2.png' alt="logo">
+        </div>
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Gallery</a></li>
+                <li><a href="#">Booking</a></li>
+                <li><a href="#">Our service</a></li>
+                <li><a href="#">About Us</a></li>
+                <li><a href="#">Contact Us</a></li>
+                <li><a href="#">My Account</a></li>
+    </header>
+    <main>
+        <form method="post" action="booking.php">
+            Name: <input type="text" name="name"><br>
+            Email: <input type="text" name="email"><br>
+            Date: <input type="date" name="date"><br>
+            Time: <input type="time" name="time"><br>
+            <input type="submit" name="submit" value="Book">
+        </form>
+    </main>
+    <footer>
+        &copy;Copyright @2023, <code>Salon Project</code>
+    </footer>
 
-<script src="https://kit.fontawesome.com/11daeabc17.js" crossorigin="anonymous"></script>
+
+    <script src="https://kit.fontawesome.com/11daeabc17.js" crossorigin="anonymous"></script>
 </body>
+
 </html>
 <?php
-    require "connection.php";
+require "connection.php";
 
-    if (isset($_POST['submit'])) {
-        $name = $_POST['name'];
-        $email = $_POST['email'];
-        $date = $_POST['date'];
-        $time = $_POST['time'];
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $date = $_POST['date'];
+    $time = $_POST['time'];
 
-        $sql = "INSERT INTO bookings (name, email, date, time) VALUES (?, ?, ?, ?)";
-        $stmt = $conn->prepare($sql);
-        $stmt->bind_param("ssss", $name, $email, $date, $time);
-        $stmt->execute();
+    $sql = "INSERT INTO bookings (name, email, date, time) VALUES (?, ?, ?, ?)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("ssss", $name, $email, $date, $time);
+    $stmt->execute();
 
-        echo "Booking successful!";
-    }
+    echo "Booking successful!";
+}
 ?>
