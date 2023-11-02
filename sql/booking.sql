90519339da358a328bdc9ad7bdba39045dc252ef
Qharny <cannycc1020@gmail.com>
2023-11-02 14:52:23 +0000

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   php/booking.php
    	modified:   sql/booking.sql

--- sql/booking.sql
+++ sql/booking.sql
@@ -3,9 +3,24 @@ use Salon_project;
 
 create table Booking
 (
+	ID int auto_increment primary key,
 	location varchar(100),
     category varchar(100),
     service varchar(100),
     date date,
     time time
 );
+
+
+
+-- Queries
+-- 1. Get all the bookings
+select * from Booking;
+-- 2. Insert a new booking
+insert into Booking (location, service) values ('Spintex', 'Hair');
+insert into Booking (location, service) values ('Circle', 'Nail');
+insert into Booking (location, service) values ('Osu', 'Pedicure');
+insert into Booking (location, service) values ('Lapaz', 'Spa');
+insert into Booking (location, service) values ('HO', 'Skin Care');
+insert into Booking (location, service) values ('Kumasi', 'Make up');
+
