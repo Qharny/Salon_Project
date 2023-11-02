6b3ea0635f89a51c28c2b593337ad367e15f5b1d
Qharny <cannycc1020@gmail.com>
2023-11-02 14:12:17 +0000

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   Booking.html
    	modified:   sql/booking.sql

--- sql/booking.sql
+++ sql/booking.sql
@@ -1,1 +1,11 @@
 -- sql for the booking page.
+use Salon_project;
+
+create table Booking
+(
+	location varchar(100),
+    category varchar(100),
+    service varchar(100),
+    date date,
+    time time
+);
