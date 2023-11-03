b77648d85e3bdc9c9efeee24001b460dceb3af1a
Qharny <cannycc1020@gmail.com>
2023-11-03 09:16:11 +0000

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   Booking.html
    	modified:   booking.css
    	modified:   login.html
    	new file:   new.html
    	modified:   php/booking.php
    	renamed:    connection.php -> php/connection.php
    	renamed:    database.php -> php/database.php
    	new file:   php/logout.php
    	renamed:    signup_database.php -> php/signup_database.php
    	modified:   sql/booking.sql

--- php/booking.php
+++ php/booking.php
@@ -1,12 +1,57 @@
 <?php
     require "./connection.php";
-    
 
+    if(isset($_POST['submit'])){
+        $cat = $_POST['category'];
+        $serve = $_POST['service'];
+        $style = $_POST['stylist'];
+        $date = $_POST['date'];
+        $time = $_POST['time'];
 
+        $query = "INSERT INTO Booking (category, stylist, service, date, time) VALUES ('$cat','$style','$serve','$date','$time')";
+        $result = mysqli_query($my_connection, $query);
+        if($result){
+            echo "<script> alert('successfully Booked');window.location('../Booking.html');</script>";
+        }
+        else{
+            echo "<script>alert('Wrong information.Try Again');window.location('../Booking.html')</script>" . mysqli_error($my_connection);
+        }
+    }
 
+    $code = "SELECT * FROM Booking";
+    $code2 = "SELECT FullName FROM signup";
+    $info = $my_connection->query($code);
+    $info2 = $my_connection->query($code2);
 
+    while($col = $info->fetch_assoc()){
+        while($col2 =$info2->fetch_assoc() ){
+        echo "
+            Mr/Miss. $col2[FullName] has successfully booked <br>
+            Category: $col[category] <br>
+            Service: $col[service] <br>
+            Stylist: $col[stylist] <br>
+            Date: $col[date] <br>
+            Time: $col[time] <br>
+            <a href='booking.php?delete_id=$col[ID]'>Delete</a> <br>
+        ";
+        }
+    }
+    if(isset($_GET['delete_id'])){
+        $delete = $_GET['delete_id'];
 
+        // prepare and execute the delete query
+        $code = "DELETE FROM signup Where ID = ?";
+        $conn = $my_connection -> prepare($code);
+        $conn-> bind_param("i", $delete);
 
+        if ($conn->execute()){
+            echo "<script >alert('Successfully deleted'); window.location='../Booking.html';</script>";
+        }else{
+            echo "<script >alert('Error');</script>";
+        }
+        // close the statement
+        $conn -> close();
+    }
 
 
 
