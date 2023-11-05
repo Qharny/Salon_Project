79f073eb6fe2404f425d69a3d7df8548b0d1abb3
may-jr <138032771+may-jr@users.noreply.github.com>
2023-11-05 03:38:41 +0000

    changes made slightly

--- php/booking.php
+++ php/booking.php
@@ -14,14 +14,14 @@
         if($result){
             //call sms api
             //https://apps.mnotify.net/smsapi?key=3w72C56POeMmp07tBxyVzS2Wj&to={}&msg=xxxxxxxx&sender_id=Saloon
-            echo "<script> alert('successfully Booked. You will recieved and SMS');window.location.href='../HTML/Booking.html';</script>";
+            echo "<script> alert('successfully Booked. You will recieved an SMS shortly');window.location.href='../HTML/Booking.html';</script>";
         }
         else{
             echo "<script>alert('Wrong information.Try Again');window.location.href='../HTML/Booking.html';</script>" . mysqli_error($my_connection);
         }
     }
 
-    $code = "SELECT * FROM Booking";
+    $code = "SELECT * FROM booking";
     $code2 = "SELECT FullName FROM signup";
     $info = $my_connection->query($code);
     $info2 = $my_connection->query($code2);
