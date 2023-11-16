f46ec3deff0bb012a8512243f6f1985bd5a684b0
QHarny <cannycc1020@gmail.com>
2023-11-16 10:07:57 +0000

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	new file:   sql/admin.sql

---
+++ sql/admin.sql
@@ -0,0 +1,17 @@
+-- Create table to accept details
+
+create table Admin (
+    AdminID int not null auto_increment,
+    AdminName varchar(50) not null,
+    AdminEmail varchar(50) not null,
+    AdminPassword varchar(50) not null,
+    primary key (AdminID)
+
+);
+
+
+-- insert into admin table
+insert into Admin (AdminNAme, AdminEmail, AdminPassword) values ('admin', 'admin@gmail.com', 'password');
+insert into Admin (AdminNAme, AdminEmail, AdminPassword) values ('Manasseh', 'manasseh@gmail.com', 'Manasseh');
+insert into Admin (AdminNAme, AdminEmail, AdminPassword) values ('MAy', 'may@gmail.com', 'mayor');
+
