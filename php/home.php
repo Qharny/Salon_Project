67cdca3275718b5be08b68993126de0d3f2103ba
QHarny <cannycc1020@gmail.com>
2023-11-17 11:33:56 +0000

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	deleted:    php/function.php
    	modified:   php/home.php
    	modified:   php/logout.php

--- php/home.php
+++ php/home.php
@@ -1,14 +1,78 @@
-<?php 
+<!DOCTYPE html>
+<html lang="en">
 
-?>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Dashboard</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            padding: 0 10px;
+        }
+        h1 {
+            color: #333;
+        }
+        #posts ul {
+            list-style-type: none;
+            padding: 0;
+        }
+        #posts ul li {
+            margin-bottom: 10px;
+            background-color: #f9f9f9;
+            padding: 10px;
+            border-radius: 5px;
+        }
+        #posts ul li:hover {
+            background-color: #e9e9e9;
+        }
+        a {
+            text-decoration: none;
+            color: #333;
+        }
+        #logout {
+            background-color: #f44336;
+            color: white;
+            padding: 10px 20px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            margin-top: 20px;
+        }
+        #logout:hover {
+            background-color: #da190b;
+        }
+        #profile-pic {
+            border-radius: 50%;
+            width: 50px;
+            height: 50px;
+        }
+    </style>
+</head>
 <body>
-    <h1>Admin Home</h1>
-    <p>Admin views customers, and manages everything here</p>
+    <h1>Welcome, Admin!</h1>
+    <img id="profile-pic" src="profile.jpg" alt="Admin Profile Picture">
+    <p>Today is <span id="date"></span>.</p>
     <section id="posts">
         <ul>
-            <li>View Users</li>
-            <li>View booking history</li>
-            <li>Access other pages directly from the dashboard</li>
+            <li><a href="users.php">View Users</a></li>
+            <li><a href="booking.php">View Booking History</a></li>
+            <li><a href="#">Manage Pages</a></li>
+            <li><a href="#">View Reports</a></li>
+            <li><a href="#">Settings</a></li>
         </ul>
     </section>
+    <a id="logout" href="logout.php">Logout</a>
+
+    <script>
+        // Get today's date
+        var today = new Date();
+        var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+        var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+        var dateTime = date+' '+time;
+
+        // Display today's date
+        document.getElementById('date').textContent = dateTime;
+    </script>
 </body>
+</html>
