1f21bbf04b4b374dbe29dcea9c1f35d756793148
QHarny <cannycc1020@gmail.com>
2023-11-17 11:08:17 +0000

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   php/delete.php

--- php/delete.php
+++ php/delete.php
@@ -1,29 +1,16 @@
 <?php
-    // Get the ID from the POST request
-    $data = json_decode(file_get_contents('php://input'), true);
-    $id = $data['id'];
-
-    // Connect to the database
     require 'connection.php';
+    if(isset($_GET['Del'])){
+        $UserID = $_GET['Del'];
 
-    // Prepare the SQL statement
-    $stmt = $my_connection->prepare("DELETE FROM booking WHERE ID = ?");
+        $query = "DELETE FROM signup WHERE ID = '$UserID'";
 
-    // Bind the ID to the SQL statement
-    $stmt->bind_param("i", $id);
-
-    // Execute the SQL statement
-    if ($stmt->execute()) {
-        // If the SQL statement executed successfully, return a success message
-        echo json_encode(['success' => true]);
-    } else {
-        // If the SQL statement failed, return an error message
-        echo json_encode(['success' => false]);
+        $result = mysqli_query($connection,$query);
+        if($result){
+            header("location:view.php");
+        }else{
+            echo "Check your query";
+        }
+    }else{
+        header("location:view.php");
     }
-
-    // Close the statement
-    $stmt->close();
-
-    // Close the connection
-    $my_connection->close();
-?>
